26f7d0560359c0eba384c09e0bddd87eb09796c6
yanyiheng13 <yanyiheng86@163.com>
2017-08-29 00:09:54 +0800

    update

--- app/src/main/java/com/community/life/ui/AuthorizationActivity.java
+++ app/src/main/java/com/community/life/ui/AuthorizationActivity.java
@@ -2,12 +2,16 @@ package com.community.life.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
 
 import com.community.life.R;
+import com.community.life.ui.view.PropertyAddressView;
 import com.community.life.ui.view.WithBackTitleView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 功能说明： 账号授权界面
@@ -19,8 +23,17 @@ import butterknife.ButterKnife;
  */
 
 public class AuthorizationActivity extends BaseActivity {
+
     @BindView(R.id.title_view)
     WithBackTitleView mTitleView;
+
+    @BindView(R.id.authorization_family_title_tv)
+    TextView mTvFamily;
+    @BindView(R.id.authorization_tenants_title_tv)
+    TextView mTvTenants;
+
+    @BindView(R.id.property_address_view)
+    PropertyAddressView mAddressView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,5 +42,23 @@ public class AuthorizationActivity extends BaseActivity {
         ButterKnife.bind(this);
         //设置账号授权界面标题icon和文字
         mTitleView.setText(R.string.account_authorise).setImageResource(R.mipmap.icon_account_authorise);
+        mAddressView.goneIcon().updateData("1");
+
+    }
+
+    @OnClick({R.id.authorization_family_title_tv, R.id.authorization_tenants_title_tv})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.authorization_family_title_tv:
+                mTvFamily.setSelected(true);
+                mTvTenants.setSelected(false);
+                break;
+            case R.id.authorization_tenants_title_tv:
+                mTvFamily.setSelected(false);
+                mTvTenants.setSelected(true);
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/community/life/ui/view/PropertyAddressView.java
+++ app/src/main/java/com/community/life/ui/view/PropertyAddressView.java
@@ -1,5 +1,6 @@
 package com.community.life.ui.view;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -27,7 +28,7 @@ import butterknife.OnClick;
  * @date: 17-8-27 下午3:46
  * @Copyright (c) 2017. Inc. All rights reserved.
  */
-
+//TODO:下面的几个方法后续整理一下 保留一个
 public class PropertyAddressView extends LinearLayout {
     //左边icon
     @BindView(R.id.property_icon_img)
@@ -69,7 +70,7 @@ public class PropertyAddressView extends LinearLayout {
         mInflater = LayoutInflater.from(context);
     }
 
-    public void updateData() {
+    public PropertyAddressView updateData() {
         mListView.clear();
         mLlAddressContain.removeAllViews();
         for (int i = 0; i < 2; i++) {
@@ -86,8 +87,9 @@ public class PropertyAddressView extends LinearLayout {
             mListView.add(view);
             mLlAddressContain.addView(view);
         }
-
+        return this;
     }
+
     public void updateData(boolean isShow) {
         mListView.clear();
         mLlAddressContain.removeAllViews();
@@ -107,10 +109,49 @@ public class PropertyAddressView extends LinearLayout {
             mListView.add(view);
             mLlAddressContain.addView(view);
         }
-
     }
 
-    private void visible() {
+    public PropertyAddressView updateData(String source) {
+        mListView.clear();
+        mLlAddressContain.removeAllViews();
+        for (int i = 0; i < 2; i++) {
+            View view = mInflater.inflate(R.layout.view_address_item_two, null);
+            TextView tvName = (TextView) view.findViewById(R.id.property_name_tv);
+            TextView tvPhone = (TextView) view.findViewById(R.id.property_phone_tv);
+            ImageView imgStatus = (ImageView) view.findViewById(R.id.property_address_status_img);//R.mipmap.icon_binded  R.mipmap.icon_not_bind
+            final TextView imgDelete = (TextView) view.findViewById(R.id.property_address_delete);//R.mipmap.icon_binded  R.mipmap.icon_not_bind
+            final ImageView imgArrow = (ImageView) view.findViewById(R.id.property_arrow_img);
+            view.setVisibility(GONE);
+            view.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (imgArrow.isEnabled()) {
+                        translationX(imgDelete);
+                    }
+                    imgArrow.setEnabled(!imgArrow.isEnabled());
+                }
+            });
+            mListView.add(view);
+            mLlAddressContain.addView(view);
+        }
+        return this;
+    }
+
+    /**
+     * 动画显示删除按钮
+     */
+    public void translationX(View view) {
+        if (view.getVisibility() == View.VISIBLE) {
+            view.setVisibility(View.GONE);
+            return;
+        }
+        view.setVisibility(View.VISIBLE);
+        ObjectAnimator transAnim = ObjectAnimator.ofFloat(view, "alpha", 0.6f, 1f);
+        transAnim.setDuration(400);
+        transAnim.start();
+    }
+
+    private PropertyAddressView visible() {
         for (View view: mListView) {
             if (mImgArrow.isEnabled()) {
                 view.setVisibility(VISIBLE);
@@ -119,10 +160,12 @@ public class PropertyAddressView extends LinearLayout {
             }
         }
         mImgArrow.setEnabled(!mImgArrow.isEnabled());
+        return this;
     }
 
-    public void goneIcon() {
+    public PropertyAddressView goneIcon() {
         mIconImg.setVisibility(View.GONE);
+        return this;
     }
 
     public void goneTxt() {
