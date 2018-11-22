6083147162fd9233b4098a23421734e4ed89b0f1
yanyiheng86 <yanyiheng86@163.com>
2018-11-22 19:03:05 +0800

    update

---
+++ app/src/main/java/com/iot12369/easylifeandroid/ui/AdActivity.java
@@ -0,0 +1,23 @@
+package com.iot12369.easylifeandroid.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.iot12369.easylifeandroid.R;
+
+/**
+ * 功能说明：广告展示界面
+ *
+ * @author: Yiheng Yan
+ * @Email: yanyiheng86@163.com
+ * @date: 17-8-26 下午7:42
+ * @Copyright (c) 2017. Inc. All rights reserved.
+ */
+public class AdActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
